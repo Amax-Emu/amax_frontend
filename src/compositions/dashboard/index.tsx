3c79353ddc19ff1c9cc08cdc1c091defdf0cbc32
Aibot <aibotnya@gmail.com>
2021-12-22 14:31:24 +0700

    tiny dashboard fix

--- src/compositions/dashboard/index.tsx
+++ src/compositions/dashboard/index.tsx
@@ -5,7 +5,7 @@ import ServerStatus from "../serverstatus";
 import LeaderboardsWidget from "../leaderboard_widget";
 import {Button, Paper} from "@material-ui/core";
 import IntroWidget from "../../components/introWidget";
-import { useAuthStore } from "../../stores"
+import {useAuthStore} from "../../stores"
 import {useUserDataStore} from "../../stores/userdataStore";
 import {Container} from "@material-ui/core";
 
@@ -15,34 +15,38 @@ export default function Dashboard() {
     const user = useUserDataStore()
 
     return (
-        <Container>
-        <div className="dashboard_container">
+        <Container maxWidth={"xl"}>
+            <div className="dashboard_container">
 
-            {user.userData?.amax_account
-                ?<></>
-                :(<div className = "main_widget_container">
-                    <IntroWidget/>
-                </div>)
-            }
-            <div className = "column1_container">
-            <SessionsWidget/>
-            </div >
-            <div className = "column2_container">
-<LeaderboardsWidget/>
-            </div>
-            <div className = "column3_container">
-<ServerStatus/>
-                {auth.signIn
-                    ?<div style={{marginTop: 200}}><iframe src="https://discord.com/widget?id=840455397818630175&theme=dark" width="350" height="500"
-                                  allowTransparency={true} frameBorder="0"
-                                  sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"/></div>
-                    :<iframe src="https://discord.com/widget?id=840455397818630175&theme=dark" width="350" height="500"
-                             allowTransparency={true} frameBorder="0"
-                             sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"/>
+                {user.userData?.amax_account
+                    ? <></>
+                    : (<div className="main_widget_container">
+                        <IntroWidget/>
+                    </div>)
                 }
+                <div className="column1_container">
+                    <SessionsWidget/>
+                </div>
+                <div className="column2_container">
+                    <LeaderboardsWidget/>
+                </div>
+                <div className="column3_container">
+                    <ServerStatus/>
+                    {auth.signIn
+                        ? <div style={{marginTop: 200}}>
+                            <iframe src="https://discord.com/widget?id=840455397818630175&theme=dark" width="350"
+                                    height="500"
+                                    allowTransparency={true} frameBorder="0"
+                                    sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"/>
+                        </div>
+                        : <iframe src="https://discord.com/widget?id=840455397818630175&theme=dark" width="350"
+                                  height="500"
+                                  allowTransparency={true} frameBorder="0"
+                                  sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"/>
+                    }
 
+                </div>
             </div>
-        </div>
         </Container>
     )
 }
